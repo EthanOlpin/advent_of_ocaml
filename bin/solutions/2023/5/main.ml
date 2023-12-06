1ead9c9ab2c290866c4a4c6716935ae21385049c
Ethan Olpin <olpin3@gmail.com>
2023-12-05 20:25:32 -0700

    improve performance of 2023 day 5 solution

--- bin/solutions/2023/5/main.ml
+++ bin/solutions/2023/5/main.ml
@@ -31,13 +31,68 @@ let solve_1 input =
 ;;
 
 let parse_seed_ranges ranges =
-  let rec aux ranges =
+  let rec aux ranges acc =
     match ranges with
-    | [ start; length ] -> [ start, length ]
-    | start :: length :: ranges -> (start, length) :: aux ranges
+    | [] -> acc
+    | start :: length :: ranges -> aux ranges ((start, start + length) :: acc)
     | _ -> failwith "Invalid seed ranges"
   in
-  aux (Parsing.ints_of_string ranges)
+  aux (Parsing.ints_of_string ranges) []
+;;
+
+let apply_map_to_range source_range sorted_map =
+  let rec aux target_range sorted_map acc =
+    match sorted_map with
+    | [] -> target_range :: acc
+    | (dest_start, source_start, length) :: rest_map ->
+      let target_start, target_stop = target_range in
+      let source_stop = source_start + length in
+      let overlap_start = max source_start target_start in
+      let overlap_stop = min source_stop target_stop in
+      if overlap_start < overlap_stop
+      then (
+        let delta = dest_start - source_start in
+        let mapped_range = overlap_start + delta, overlap_stop + delta in
+        (* If we encounter an overlap we know any left-hanging segment
+           cannot be applied to a range later in the map because the map
+           is sorted. Map the overlapping segment to dest, if there's a
+           hanging segment to the right of the overlap, keep looking to
+           see if it overlaps with a range later in the map*)
+        let acc' =
+          if target_start < overlap_start
+          then (target_start, overlap_start) :: mapped_range :: acc
+          else mapped_range :: acc
+        in
+        if overlap_stop < target_stop
+        then aux (overlap_stop, target_stop) rest_map acc'
+        else acc')
+      else aux target_range rest_map acc
+  in
+  aux source_range sorted_map []
+;;
+
+let apply_maps_to_ranges source_ranges sorted_maps =
+  let rec aux sorted_maps source_ranges =
+    match sorted_maps with
+    | [] -> source_ranges
+    | map :: rest_maps ->
+      List.concat_map source_ranges ~f:(fun source_range ->
+        apply_map_to_range source_range map)
+      |> aux rest_maps
+  in
+  aux sorted_maps source_ranges
+;;
+
+let min_of_ranges ranges =
+  List.fold ranges ~init:None ~f:(fun acc (start, _) ->
+    match acc with
+    | None -> Some start
+    | Some acc -> Some (min acc start))
+  |> Option.value_exn
+;;
+
+let range_compare (_, source_start, _) (_, source_start', _) =
+  Int.compare source_start source_start'
 ;;
 
 let solve_2 input =
@@ -45,16 +100,12 @@ let solve_2 input =
   | [] -> failwith "Empty input"
   | seed_ranges :: maps ->
     let seed_ranges = parse_seed_ranges seed_ranges in
-    let maps = List.map maps ~f:parse_map in
-    List.fold
-      seed_ranges
-      ~init:Int.max_value
-      ~f:(fun curr_min (start, length) ->
-        (* Lazy evaluation is the only thing standing between us and running out of memory *)
-        Sequence.range start (start + length)
-        |> Sequence.fold ~init:curr_min ~f:(fun curr_min seed ->
-          let location = List.fold maps ~init:seed ~f:apply_map in
-          min curr_min location))
+    let sorted_maps =
+      List.map maps ~f:(fun map ->
+        parse_map map |> List.sort ~compare:range_compare)
+    in
+    apply_maps_to_ranges seed_ranges sorted_maps
+    |> min_of_ranges
     |> Int.to_string
 ;;
 
